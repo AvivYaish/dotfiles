88af131cff61766485bdd28fb49aeb178aaa8e3c
Aviv <aviv.yaish@mail.huji.ac.il>
2022-04-21 15:22:29 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,3 @@
-# Installation
-
-# I put non-packaged apps in ${HOME}\.local\bin, and add the path to PATH.
-# Manual installation:
-# - [reader](https://github.com/mrusme/reader) 	# CLI webpage reader.
-
-# foreach ($app in @('JanDeDobbeleer.OhMyPosh')) {
-# 	winget install $app
-# }
-
-# Install-Module PSWindowsUpdate 	# Update windows using 'Get-WindowsUpdate ; Install-WindowsUpdate'
-# Install-Module PSReadLine		# Nicer PowerShell auto-completion
-# Install-Module -Name PSFzf 		# fzf integration with PowerShell
-# foreach ($app in @('PSWindowsUpdate', 'PSReadLine', 'PSWindowsUpdate', 'PSFzf')) {
-# 	Install-Module $app
-# }
-# iwr -useb get.scoop.sh | iex		# Install 'scoop' package manager, requires first setting execution policies, e.g. 'Set-ExecutionPolicy RemoteSigned -Scope CurrentUser'
-# foreach ($app in @('sudo', '7zip', 'chezmoi', 'bat', 'lsd', 'ripgrep', 'sd', 'universal-ctags', 'neovim', 'zoxide', 'fd', 'tldr', 'fzf', 'lf', 'curlie', 'procs', 'bottom', 'tre-command', 'glow')) {
-# 	scoop install $app
-# }
-# # visidata is a nice terminal spreadsheet tool, requires windows-curses
-# foreach ($app in @('ptpython', 'visidata', 'windows-curses', 'epy-reader')) {
-# 	pip install $app
-# }
-# foreach ($app in @('viu')) {
-# 	cargo install $app
-# }
-
 Set-PoshPrompt -Theme ~\.mytheme.omp.json # Set theme (uses oh-my-posh)
 
 # Aliases
