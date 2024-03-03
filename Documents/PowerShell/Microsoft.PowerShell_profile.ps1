ca04b65f1a593af2151590f80267be53aa35a351
Aviv Yaish <12000894+AvivYaish@users.noreply.github.com>
2024-03-03 16:49:00 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,11 @@ Invoke-Expression (& {$hook = if ($PSVersionTable.PSVersion.Major -lt 6) {'promp
 
 # Aliases
 function ..() {cd ..}
+Set-Alias -Name ls -Value lsd
+Set-Alias -Name du -Value dust
 Set-Alias -Name cut -Value hck
 Set-Alias -Name cat -Value bat
 Set-Alias -Name sed -Value sd
-Set-Alias -Name ls -Value lsd
 Set-Alias -Name vim -Value nvim
 Set-Alias -Name tldr -Value tealdeer-windows-x86_64-msvc
 function ll([String[]] [Parameter(Position=1, ValueFromRemainingArguments)] $Remaining) {lsd -lhat $Remaining}
